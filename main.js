cb4741bc38867b190d83a735fe239207af622573
Al Lasco <avlasco@voyagerinnovation.com>
2015-11-24 19:11:58 +0800

    added support for user-defined regexes

--- main.js
+++ main.js
@@ -7,12 +7,19 @@ define(['vladinator'], function (Vlad) {
   var vlad = new Vlad({
     el: 'form1',
     elements: {
-      phoneNumber: [
-        {type: 'phone', message: 'Enter a valid phone number'}
-      ],
-      email: [
-        {type: 'email', message: 'Enter a valid email address'}
-      ]
+      phoneNumber: [{
+        type: 'phone',
+        message: 'Enter a valid phone number'
+      }],
+      email: [{
+        type: 'email',
+        message: 'Enter a valid email address'
+      }],
+      cellphone: [{
+        type: 'philippinePhoneNumber',
+        message: 'Enter a valid Philippine cellphone number',
+        regex: /((\+63)|0)\d{10}/
+      }]
     }
 
   });
